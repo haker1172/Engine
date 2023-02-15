742aa41b5d8366b494ba3034d35960d2927fe1d6
haker1172 <ivanyska9@gmail.com>
2023-02-15 13:02:09 +0300

    Update

---
+++ Engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Engine applications
+
+#include <stdio.h>
+#include "Engine/Application.h"
+//---EntryPoint---------------------
+#include "Engine/EntryPoint.h"
+//----------------------------------
