e55681fce92c62e201675fcb7f7003f43947cc9f
haker1172 <102350987+haker1172@users.noreply.github.com>
2023-02-15 10:59:12 +0300

    Delete Engine directory

--- Engine/src/Engine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by Engine applications
-
-#include <stdio.h>
-#include "Engine/Application.h"
-//---EntryPoint---------------------
-#include "Engine/EntryPoint.h"
-//----------------------------------
